470c8b56933e2b696c543bd6244dc2405fefb690
Jannik0308 <jnkarger01@gmail.com>
2022-08-01 15:35:13 +0200

    Added warn level to logger class

--- src/main/java/de/minefactprogress/progressplugin/utils/Logger.java
+++ src/main/java/de/minefactprogress/progressplugin/utils/Logger.java
@@ -3,11 +3,16 @@ package de.minefactprogress.progressplugin.utils;
 import com.google.gson.JsonObject;
 import de.minefactprogress.progressplugin.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class Logger {
 
     public static void info(String message) {
         Bukkit.getConsoleSender().sendMessage(Main.getPREFIX() + message);
+    }
+
+    public static void warn(String message) {
+        Bukkit.getLogger().warning(Main.getPREFIX() + ChatColor.stripColor(message));
     }
 
     public static void error(String message) {
