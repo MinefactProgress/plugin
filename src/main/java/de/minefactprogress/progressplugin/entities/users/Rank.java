671f1f8ad813b59bf4ef79aa8c3250e8886c3da5
Jannik0308 <hightower3801@gmail.com>
2023-05-21 11:29:48 +0200

    Possible Admin rank detection fix

--- src/main/java/de/minefactprogress/progressplugin/entities/users/Rank.java
+++ src/main/java/de/minefactprogress/progressplugin/entities/users/Rank.java
@@ -74,11 +74,10 @@ public enum Rank {
     }
 
     public static Rank getByPermission(Player p) {
-        if(p.getUniqueId().toString().equals("ac10ddeb-c65a-408e-a036-f7a7e51254f4")) {
-            return OWNER;
-        }
         for(Rank rank : Rank.values()) {
-            if(rank == OWNER) continue;
+            if((rank == OWNER || rank == ADMINISTRATOR) && Permissions.hasPermission(p, rank.permission)) {
+                return p.getUniqueId().toString().equals("ac10ddeb-c65a-408e-a036-f7a7e51254f4") ? OWNER : ADMINISTRATOR;
+            }
 
             if(Permissions.hasPermission(p, rank.permission)) {
                 return rank;
